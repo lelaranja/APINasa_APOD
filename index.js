7d6215a6ec13af5685bf4e788d2d4c5d1d5014e9
lelaranja <leticiajlaranja@gmail.com>
2022-05-09 16:07:19 -0300

    Site finalizado e media query implementada

---
+++ index.js
@@ -0,0 +1,28 @@
+$('#submit').click(function () {
+    var vlData = $('#data').val();
+    console.log(vlData)
+    $.ajax({
+        url: `https://api.nasa.gov/planetary/apod?api_key=8AGE5N7zUVGcasuuqBL0inBRkd7VYnSTrZ1ste1A&date=${vlData}`,
+        type: 'GET',
+        success: function (dado) {
+            console.log(dado);
+            $('#tituloImg').text(dado.title);
+            $('#resultImg').css('display', 'none');
+            $('#vidYT').css('display', 'none');
+
+            if (dado.media_type == 'video') {
+                $('#vidYT').attr('src', dado.url);
+                $('#vidYT').css('display', 'flex');
+            } else {
+                $('#resultImg').attr('src', dado.url);
+                $('#resultImg').css('display', 'flex');
+                $('#resultImg').css({
+                    "border-color": "white",
+                    "border-width": "3px",
+                    "border-style": "solid"
+                });
+            }
+            $('#explicacao').text(dado.explanation);
+        },
+    });
+});
